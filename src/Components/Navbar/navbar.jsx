4a4b21ee135c4489252cef7e3590f9bf642185a1
Adriano <adrianotaua@hotmail.com>
2024-01-28 17:56:16 -0300

    Histórico de pedidos e Prop btn Histórico no Menu

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -2,8 +2,10 @@ import "./navbar.css";
 import logo from "../../Assets/logo.png";
 import bag from "../../Assets/bag.png";
 import Cart from "../Cart/cart";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
+
   function openSidebar() {
     const event = new CustomEvent("openSidebar");
     window.dispatchEvent(event);
@@ -13,10 +15,14 @@ function Navbar(props) {
     <div className="navbar">
       <img src={logo} className="logotipo" alt="Logotipo" />
 
-      {/* props.showMenu && */}
+      {/* Decisão simples ou usando: props.showMenu && */}
       {props.showMenu ? (
         <div className="menu">
-          <a href="/historico">Histórico</a>
+					{/* Só exibe histórico fora da página histórico */}
+          {/* LINK: Transição entre telas sem refresh E Navigate*/}
+          {props.showHistorico && 
+						<Link to="/historico">Histórico</Link>
+					}
           <button onClick={openSidebar} className="btn btn-red">
             <img className="icon" src={bag} alt="Sacola" />
             Sacola
--- src/Pages/Historico/historico.jsx
+++ src/Pages/Historico/historico.jsx
@@ -1,9 +1,66 @@
-// import "./historico.css";
+import Navbar from "../../Components/Navbar/navbar";
+import "./historico.css";
 
 function Historico() {
   return (
     <>
-      <h1>Página de Histórico...</h1>
+      <Navbar showMenu={true} showHistorico={false} />
+
+      <div className="container">
+        <div className="titulo text-center">
+          <h1>Histórico de pedidos</h1>
+        </div>
+
+        <div className="box-pedido">
+          <table className="table">
+            <tr>
+              <th>Nº PEDIDO</th>
+              <th>DATA</th>
+              <th>VALOR</th>
+            </tr>
+
+            <tr className="text-center">
+              <td>
+                <strong>Pedido: 51354</strong>
+              </td>
+              <td className="text-light">02/01/2024</td>
+              <td className="text-red">R$ 123,00</td>
+            </tr>
+
+            <tr className="text-center">
+              <td>
+                <strong>Pedido: 51354</strong>
+              </td>
+              <td className="text-light">12/01/2024</td>
+              <td className="text-red">R$ 133,00</td>
+            </tr>
+
+            <tr className="text-center">
+              <td>
+                <strong>Pedido: 51354</strong>
+              </td>
+              <td className="text-light">14/01/2024</td>
+              <td className="text-red">R$ 120,00</td>
+            </tr>
+
+            <tr className="text-center">
+              <td>
+                <strong>Pedido: 51354</strong>
+              </td>
+              <td className="text-light">20/01/2024</td>
+              <td className="text-red">R$ 98,00</td>
+            </tr>
+
+            <tr className="text-center">
+              <td>
+                <strong>Pedido: 51354</strong>
+              </td>
+              <td className="text-light">27/01/2024</td>
+              <td className="text-red">R$ 88,00</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
